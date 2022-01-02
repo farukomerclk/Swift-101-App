a00a81d697893ffea58b3daccf2be397da5106df
farukomerclk <farukomer7000@gmail.com>
2022-01-02 15:38:14 +0300

    Initial Commit

---
+++ Swift-101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-101
+//
+//  Created by Ömer Faruk Çelik on 2.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-101/Swift_101App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_101App.swift
+//  Swift-101
+//
+//  Created by Ömer Faruk Çelik on 2.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
