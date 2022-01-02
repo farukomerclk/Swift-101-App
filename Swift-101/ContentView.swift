2844bb7dbd1fab9e4bb0029b4831170615eb6ffb
farukomerclk <farukomer7000@gmail.com>
2022-01-02 23:08:49 +0300

    First commit

--- Swift-101/ContentView.swift
+++ Swift-101/ContentView.swift
@@ -4,18 +4,19 @@
 //
 //  Created by Ömer Faruk Çelik on 2.01.2022.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        let x = "huu"
+        
+        let str: String = "type inference"
+        
+        let array:[Int] = [4,56,7,8]
+        var liste = (["item1", "item2", "item3", "item4",])
+        
+        Text(str)
+        Text("Hello, \(x)!")
+        Text("item listesi: \(liste[0])")
     }
 }
